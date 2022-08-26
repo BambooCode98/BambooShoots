236b8110e580d8099d4d3508611054e87b4c549c
BambooCode98 <jw.thomas98@yahoo.com>
2022-08-26 11:22:43 -0500

    minor changes

--- components/ProfileLayout.js
+++ components/ProfileLayout.js
@@ -95,7 +95,7 @@ export default function ProfileLayout({children}) {
             </div>
           </div>
           <div className={modalDisplay}>
-            <p className={finishUploading ? showUploadClear : closeUploadClear}>Upload Successful, Refresh Page to view photos!</p>
+            <p className={finishUploading ? showUploadClear : closeUploadClear}>Upload Successful, Refresh to view photos!</p>
             <Link href='/account'>
               <a>
                 <h1 className='cursor-pointer flex gap-2 text-green-400 w-auto sm:w-auto justify-center text-2xl md:text-4xl font-bold border-b-2 border-y-2 border-black'>BambooShoots
