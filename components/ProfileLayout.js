82865050eb1de950827ab70578c3c21cca0bd930
BambooCode98 <jw.thomas98@yahoo.com>
2022-08-26 09:19:57 -0500

    minor changes

--- components/ProfileLayout.js
+++ components/ProfileLayout.js
@@ -49,9 +49,9 @@ export default function ProfileLayout({children}) {
             method: 'POST',
             body: imageBinary
           }).then(() => {setfinishUploading(true)})
-          // .then( res => {
-          //   res? location.href='https://bamboo-shoots.vercel.app/account/photo' : null;
-          // })
+          .then( res => {
+            res? location.href='https://bamboo-shoots.vercel.app/account/photo' : null;
+          })
         }
         reader.onerror = (error) => {
           // console.log(reader.error);
