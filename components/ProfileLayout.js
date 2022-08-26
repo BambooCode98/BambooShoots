6488f0f1990f93752eb0c1ee5e117ca7b4082fa4
BambooCode98 <jw.thomas98@yahoo.com>
2022-08-26 11:08:24 -0500

    minor changes

--- components/ProfileLayout.js
+++ components/ProfileLayout.js
@@ -51,6 +51,8 @@ export default function ProfileLayout({children}) {
           }).then(() => {
             setfinishUploading(true);
             setFiles([]);
+            setSelectedFiles(false);
+
           })
           
         }
